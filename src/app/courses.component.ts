6add33751e8b131695903a59c48e36dfce5f4175
Younous <Younissadam2020@gmail.com>
2021-05-26 10:11:46 -0400

    Directive and altenating through and array

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: '<h2>{{title}}</h2>',
+  template: `<h2>{{ getTitle() }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul> `,
 })
 export class CoursesComponent {
   title = 'List of courses';
+  courses = [' course1', 'course2', 'course3'];
 
   getTitle() {
     return this.title;
   }
 }
 
-// With Angular CLI
-// The  best way to generate a component
-// ng g c course
+// Directive
+// *ngFor="take an expression"
+// let course of courses ====> we defining a variable, this is like a loop variable in javascript
+// when angular see this is going to Alternate throught our courses array
