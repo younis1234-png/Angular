310759929b012f2ec5199f15a21b3452ed903783
Younous <Younissadam2020@gmail.com>
2021-05-26 09:44:16 -0400

    Create a component, add it to dekacation array in app. module, and call it inside the template app.comp[onent.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  // here where we add all our component
+  declarations: [AppComponent, CoursesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
